5cd94f1520a4390cedb4dafbd3a6fd102a13f898
chrishoynck <94909944+chrishoynck@users.noreply.github.com>
2025-02-24 12:32:32 +0100

    initial setup, add environment to gitignore

---
+++ src/visualizations.py
@@ -0,0 +1,6 @@
+import matplotlib.cm as cm
+import matplotlib.colors as mcolors
+import matplotlib.pyplot as plt
+import numpy as np
+import src.solutions as solutions
+from matplotlib.animation import FuncAnimation
